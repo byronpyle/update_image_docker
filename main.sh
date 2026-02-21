fb8a1031d856d4ced76a00583e47c0f55d76f9fa
byronpyle <byron4200@gmail.com>
2026-02-21 12:56:17 -0500

    Update curl commands in main.sh for API requests

--- main.sh
+++ main.sh
@@ -1,3 +1,7 @@
-curl -X POST \
+jq -Rs '{StackFileContent: .}' docker-compose.yml > payload.json
+
+curl -X PUT \
   -H "X-API-Key: $PORTAINER_API_KEY" \
-  "$PORTAINER_URL/api/stacks/$PORTAINER_STACK_ID/git-pull?force=true"
+  -H "Content-Type: application/json" \
+  --data-binary "@payload.json" \
+  "$PORTAINER_URL/api/stacks/$PORTAINER_STACK_ID?endpointId=$PORTAINER_ENDPOINT_ID"
