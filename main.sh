f16fb4229eebf0bb0b19c7dc961c1511eb88dfb4
byronpyle <byron4200@gmail.com>
2026-02-21 13:00:27 -0500

    Update and rename main.sh to main.yml
    Move to yml file to update, embed bash in yml

--- main.sh
+++
@@ -1,7 +0,0 @@
-jq -Rs '{StackFileContent: .}' docker-compose.yml > payload.json
-
-curl -X PUT \
-  -H "X-API-Key: $PORTAINER_API_KEY" \
-  -H "Content-Type: application/json" \
-  --data-binary "@payload.json" \
-  "$PORTAINER_URL/api/stacks/$PORTAINER_STACK_ID?endpointId=$PORTAINER_ENDPOINT_ID"
